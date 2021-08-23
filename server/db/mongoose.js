06460616604ad6f5a8108139173de802c4c66900
AlbinoRampinelli <78177581+AlbinoRampinelli@users.noreply.github.com>
2021-08-23 19:56:28 -0300

    add mongodb

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 let mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.NGODB_URI || 'mongodb://localhost:27017/Todo', {userNewUrlParser: true });
+mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/Todo', {userNewUrlParser: true }, () => console.log('Conneted to Database') );
 
 module.exports = {
     mongoose
